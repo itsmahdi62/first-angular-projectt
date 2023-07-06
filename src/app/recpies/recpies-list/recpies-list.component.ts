2cec0738b91436f42ddc1a781b52805e392ba74e
Mahdi <amiralmasi021@gmail.com>
2023-07-07 01:15:47 +0330

    Create first Recipe post

--- src/app/recpies/recpies-list/recpies-list.component.ts
+++ src/app/recpies/recpies-list/recpies-list.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recpies-list',
   templateUrl: './recpies-list.component.html',
   styleUrls: ['./recpies-list.component.css']
 })
 export class RecpiesListComponent {
+    recipes : Recipe[] = [
+      new Recipe('A Test Recipe' , 'This is simply a test',"https://food.fnr.sndimg.com/content/dam/images/food/fullset/2012/3/14/0/0181847_pork-chops_s4x3.jpg.rend.hgtvcom.476.357.suffix/1393206943617.jpeg")
+    ] ; 
 
 }
