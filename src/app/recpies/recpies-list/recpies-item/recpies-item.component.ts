c94b08b6fade47f95d6d9da90d10cbaa296625c3
Mahdi <amiralmasi021@gmail.com>
2023-07-11 15:10:55 +0330

    Create a component for each recpies

--- src/app/recpies/recpies-list/recpies-item/recpies-item.component.ts
+++ src/app/recpies/recpies-list/recpies-item/recpies-item.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recpies-item',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./recpies-item.component.css']
 })
 export class RecpiesItemComponent {
-
+  @Input() recipe!: Recipe; 
 }
