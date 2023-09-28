6959669568873e694c94f4053f1d3c8df4badb66
Mahdi <amiralmasi021@gmail.com>
2023-09-29 00:31:08 +0330

    Debug

--- src/app/recpies/recpies-detail/recpies-detail.component.ts
+++ src/app/recpies/recpies-detail/recpies-detail.component.ts
@@ -2,27 +2,28 @@ import { Component, Input } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recpies-list/recipe.service';
 import { ActivatedRoute } from '@angular/router';
-import { Params } from 'react-router-dom';
+import { Params } from '@angular/router';
 
 @Component({
   selector: 'app-recpies-detail',
   templateUrl: './recpies-detail.component.html',
   styleUrls: ['./recpies-detail.component.css']
 })
-
 export class RecpiesDetailComponent {
  recipe !: Recipe; 
- id: number | any; 
+   id !: number ; 
 
   constructor(
     private recipeService: RecipeService,
-    private route: ActivatedRoute){}
+    private route: ActivatedRoute){
+      
+    }
 
 
     ngOnInit(){
       this.route.params.subscribe(
         (params:Params) =>{
-          this.id = params['id'];
+          this.id += params['id'];
         }
       )
     }
