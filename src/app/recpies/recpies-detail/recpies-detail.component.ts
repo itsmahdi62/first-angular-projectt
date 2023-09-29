48f3e41783eb46db0e9ae39823950d140a1a619a
Mahdi <amiralmasi021@gmail.com>
2023-09-29 21:43:10 +0330

    D

--- src/app/recpies/recpies-detail/recpies-detail.component.ts
+++ src/app/recpies/recpies-detail/recpies-detail.component.ts
@@ -1,8 +1,5 @@
 import { Component, Input } from '@angular/core';
 import { Recipe } from '../recipe.model';
-import { RecipeService } from '../recpies-list/recipe.service';
-import { ActivatedRoute } from '@angular/router';
-import { Params } from 'react-router-dom';
 
 @Component({
   selector: 'app-recpies-detail',
@@ -10,19 +7,5 @@ import { Params } from 'react-router-dom';
   styleUrls: ['./recpies-detail.component.css']
 })
 export class RecpiesDetailComponent {
- recipe !: Recipe; 
- id: number | any; 
-
-  constructor(
-    private recipeService: RecipeService,
-    private route: ActivatedRoute){}
-
-
-    ngOnInit(){
-      this.route.params.subscribe(
-        (params:Params) =>{
-          this.id = params['id'];
-        }
-      )
-    }
+  @Input() recipe!: Recipe; 
 }
